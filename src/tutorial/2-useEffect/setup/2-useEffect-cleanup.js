5921ecef5b3a96c7748102ba4c8a4357195dc8e2
IMRANDIL <aliimranadil2@gmail.com>
2021-06-18 19:22:34 +0530

    yh

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,26 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const windowResize = () =>{
+    setSize(window.innerWidth)
+  }
+
+useEffect(()=>{
+  window.addEventListener('resize', windowResize);
+  return ()=>{
+    console.log('cleanup');
+    window.removeEventListener('resize', windowResize)
+  }
+},[])
+
+
+
+  return <>
+  <h2>window size</h2>
+  <h4>{size} px</h4>
+  </>
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,38 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+const [profile, setProfile] = useState([]);
+const fetchData = async () =>{
+  const response = await fetch(url);
+  const data = await response.json();
+  setProfile(data)
+ 
+}
+useEffect(()=>{
+  fetchData()
+
+},[])
+
+  return <>
+  <h2>Github users</h2>
+  <ul className='users'>
+{profile.map((person)=>{
+  const {id,login,avatar_url,html_url} = person;
+  return (
+  <li key={id}>
+    <img src={avatar_url} alt={login} />
+    <div>
+      <h4>{login}</h4>
+      <a href={html_url}>User_profile</a>
+    </div>
+  </li>
+  )
+})}
+
+
+  </ul>
+
+  </>
 };
 
 export default UseEffectFetchData;
