610867e97f4060171b60c860bcfd049b9099caf3
IMRANDIL <aliimranadil2@gmail.com>
2021-06-20 10:58:18 +0530

    yh

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,24 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+<h1>{text || 'hello world'}</h1>
+
+<button className='btn' onClick={()=>setIsError(!isError)}>show error</button>
+
+<h1>{isError && 'Error Occurred..'}</h1>
+
+{isError ? (<p>There is an Error..</p>) : (<div><h2>There is no Error..</h2></div>)}
+
+
+    </>
+  )
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return <>
+    <button className='btn' onClick={() => setShow(!show)}> show/hide</button>
+
+
+    {show && <Item />}
+
+  </>
 };
+
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const setWindowSize = () => {
+    setSize(window.innerWidth);
+  }
+
+  useEffect(() => {
+
+
+    window.addEventListener('resize', setWindowSize);
+
+
+    return () => {
+      window.removeEventListener('resize', setWindowSize)
+    }
+
+  }, [])
+
+
+
+  return (
+    <div>
+      <h1>Window</h1>
+
+      <h3 style={{marginTop:'2rem',color:'red'}}>width : {size} px</h3>
+
+
+
+
+    </div>
+  )
+}
+
 
 export default ShowHide;
