a67ed12a12ae036c9d63e3d3a7333bc06079f1d9
IMRANDIL <aliimranadil2@gmail.com>
2021-06-27 14:20:41 +0530

    yh

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,22 +3,39 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+
+
+const PersonContext = React.createContext();
+
+// two components - provider, consumer
+
+
+
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
+
   const removePerson = (id) => {
     setPeople((people) => {
       return people.filter((person) => person.id !== id);
     });
   };
+
+
+
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{removePerson,people}}>
+      <h3>context APi / useContext</h3>
+      <List  />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+
+
+const List = () => {
+  const {people} = useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
@@ -26,7 +43,7 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
+           
           />
         );
       })}
@@ -34,7 +51,12 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+
+
+
+const SinglePerson = ({ id, name }) => {
+  const {removePerson} = useContext(PersonContext);
+  
   return (
     <div className='item'>
       <h4>{name}</h4>
